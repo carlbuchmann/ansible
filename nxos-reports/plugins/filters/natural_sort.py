937838086158669b2f49f11ab58a00898ba69464
carlbuchmann <carl.buchmann@gmail.com>
2017-11-22 21:45:44 -0500

    nat_sort filter

---
+++ nxos-reports/plugins/filters/natural_sort.py
@@ -0,0 +1,21 @@
+#
+# natural_sort filter
+#
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from jinja2 import TemplateError
+
+import re
+
+class FilterModule(object):
+
+  def natural_sort(l):
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ]
+    return sorted(l, key = alphanum_key)
+
+  def filters(self):
+    return {
+      'natural_sort' : self.natural_sort,
+    }
