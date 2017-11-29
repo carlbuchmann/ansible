13931dc759a8dd3825102170fcc5630da4b5e3f4
carlbuchmann <carl.buchmann@gmail.com>
2017-11-28 20:35:53 -0500

    nx-os documentation sort filter fix

--- nxos-reports/plugins/filters/natural_sort.py
+++ nxos-reports/plugins/filters/natural_sort.py
@@ -1,19 +1,16 @@
 #
 # natural_sort filter
 #
-from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
-
 from jinja2 import TemplateError
 
 import re
 
 class FilterModule(object):
 
-  def natural_sort(self,1):
+  def natural_sort(self,el):
     convert = lambda text: int(text) if text.isdigit() else text.lower()
     alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ]
-    return sorted(l, key = alphanum_key)
+    return sorted(el, key = alphanum_key)
 
   def filters(self):
     return {
